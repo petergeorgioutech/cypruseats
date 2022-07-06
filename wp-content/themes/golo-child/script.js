4fcb02282a294b55473bd30c82adb439edc6a9ab
Peter Georgiou <peter@petergeorgiou.tech>
2022-07-06 11:56:38 +0300

    Disable typing on the search bar

--- wp-content/themes/golo-child/script.js
+++ wp-content/themes/golo-child/script.js
@@ -1,4 +1,34 @@
 'use strict';
-jQuery( document ).ready( function($) {
+(function($) {
 
-} );
+    disableSearchType();
+
+    var $where  = $("#find_city"),
+        $find   = $("#find_input");
+
+    $where.click(function() {    
+        //clear value
+        $("input[name=ciid]").attr("value", "");
+
+        $(".location-field .area-result").hide();
+        $(".location-field .focus-result").show();
+    });
+    
+    $find.click(function() {    
+        //clear value
+        $("input[name=ciid]").attr("value", "");
+
+        if ( $(".input-field .search-result").is(':visible') ) {
+            $("#find_input").removeAttr("readonly").css("cursor", "text");
+        }
+        else {
+            $("#find_input").attr("readonly", "").css("cursor", "default");
+        }
+    });
+
+    function disableSearchType() {
+        $("#find_city").attr("readonly", "").css("cursor", "default");
+        $("#find_input").attr("readonly", "").css("cursor", "default");
+    }
+
+})( jQuery );
