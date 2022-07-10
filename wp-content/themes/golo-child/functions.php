62cb1394c690322f06f20862de1cdc424f5dba50
Peter Georgiou <peter@petergeorgiou.tech>
2022-07-10 20:34:00 +0300

    missing style scss and generated php for custom field

--- wp-content/themes/golo-child/functions.php
+++ wp-content/themes/golo-child/functions.php
@@ -20,3 +20,63 @@ if ( ! function_exists( 'golo_child_enqueue_scripts' ) ) {
 	}
 
 }
+
+/**
+ * Custom Fields
+ */
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_62c83c2925e86',
+	'title' => 'Related Content',
+	'fields' => array(
+		array(
+			'key' => 'field_62c83c30a2cad',
+			'label' => 'Content Selection',
+			'name' => 'content_selection',
+			'type' => 'relationship',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'post_type' => array(
+				0 => 'post',
+			),
+			'taxonomy' => '',
+			'filters' => array(
+				0 => 'search',
+			),
+			'elements' => array(
+				0 => 'featured_image',
+			),
+			'min' => '',
+			'max' => '',
+			'return_format' => 'object',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'place',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => 0,
+));
+
+endif;		
