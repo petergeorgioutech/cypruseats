aea4fc03455d2c00fd568dc173e06336d06df034
Peter Georgiou <peter@petergeorgiou.tech>
2022-07-11 14:02:07 +0300

    remove clearing of attribute for search forms

--- wp-content/themes/golo-child/script.js
+++ wp-content/themes/golo-child/script.js
@@ -10,7 +10,7 @@
 
     $whereWrapper.click(function() {    
         //clear value
-        $("#wrapper input[name=ciid]").attr("value", "");
+        // $("#wrapper input[name=ciid]").attr("value", "");
 
         $("#wrapper .location-field .area-result").hide();
         $("#wrapper .location-field .focus-result").show();
@@ -18,7 +18,7 @@
     
     $findWrapper.click(function() {    
         //clear value
-        $("#wrapper input[name=ciid]").attr("value", "");
+        // $("#wrapper input[name=ciid]").attr("value", "");
 
         if ( $("#wrapper .input-field .search-result").is(':visible') ) {
             $("#wrapper #find_input").removeAttr("readonly").css("cursor", "text");
@@ -30,7 +30,7 @@
     
     $wherePopup.click(function() {    
         //clear value
-        $(".search-form-wrapper input[name=ciid]").attr("value", "");
+        // $(".search-form-wrapper input[name=ciid]").attr("value", "");
 
         $(".search-form-wrapper .location-field .area-result").hide();
         $(".search-form-wrapper .location-field .focus-result").show();
@@ -38,7 +38,7 @@
     
     $findPopup.click(function() {    
         //clear value
-        $(".search-form-wrapper input[name=ciid]").attr("value", "");
+        // $(".search-form-wrapper input[name=ciid]").attr("value", "");
 
         if ( $(".search-form-wrapper .input-field .search-result").is(':visible') ) {
             $(".search-form-wrapper #find_input").removeAttr("readonly").css("cursor", "text");
